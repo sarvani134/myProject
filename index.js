8ef4b056fdf7a8503783ac8a33d66a0083f50504
sarvani <modumudisarvani@gmail.com>
2026-01-25 15:58:31 +0530

    add changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const app=express()
+const mongoose=require("mongoose")
 const path=require("path")
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
